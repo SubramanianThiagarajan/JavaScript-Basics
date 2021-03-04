5053a162daad6bbc0bfafbb7a0c70f391445da57
Subramanian Thiagarajan <subramanian30799@gmail.com>
2021-03-04 21:23:08 +0530

    Variables and Constants done

---
+++ variables.js
@@ -0,0 +1,12 @@
+let age = 100;
+
+let firstName = "James";
+let lastName = "Bond";
+
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName + ' is ' + age + ' years old');
+
+const number = 15;
+
+//number = 20; Not possible
